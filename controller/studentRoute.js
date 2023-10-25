3e23129f79a9a3e95df42b8c23b6140ea8318ebb
Suyash Pandey <132813748+SuyashPandey30@users.noreply.github.com>
2023-10-25 09:33:06 +0530

    Update studentRoute.js

--- controller/studentRoute.js
+++ controller/studentRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const studentSchema = require("../model/studentschema");
+const studentSchema = require("./Model/studentschema");
 const mongoose = require("mongoose");
 
 const studentRoute = express.Router();
