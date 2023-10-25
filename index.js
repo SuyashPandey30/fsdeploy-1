014a518bb988c9b745bad2099a199e37f8c90d2a
Suyash Pandey <132813748+SuyashPandey30@users.noreply.github.com>
2023-10-25 09:53:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const studentRoute = require("./controller/studentRoute");
+const studentSchema = require("../Model/studentschema.js");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
